f542bb543fdf9a4723b990270b2f6b2dc33719d0
Mr_Shubham_Khalane <118313700+1khalaneshubham@users.noreply.github.com>
2024-05-01 02:05:32 +0530

    Add files via upload

---
+++ Fruits/index.js
@@ -0,0 +1,8 @@
+
+const apple = require("./apple");
+const banana = require("./banana");
+const mango = require("./mango");
+
+let fruits = [apple,banana,mango];
+
+module.exports = fruits;
---
+++ Fruits/mango.js
@@ -0,0 +1,6 @@
+
+
+module.exports = {
+    name:"mango",
+    color:"orange",
+};
