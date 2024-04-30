f5f7db53f0b78cb6a3e292fa1f3ac007f883d56d
Mr_Shubham_Khalane <118313700+1khalaneshubham@users.noreply.github.com>
2024-05-01 02:10:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+// import {sum,PI} from "./math.js";
+import { generate} from "random-words";
+console.log(generate());
+
+// console.log(sum(1,2));
+// // let  n = 5;
+
+// // for(let i = 0; i<n; i++){
+// //     console.log("Welcome to Node",i);
+// // }
+
+// // console.log("Oky tata by by !")
+
+// // console.log(process.argv);
+
+// let args = process.argv;
+
+// for(let i = 2; i<args.lenght; i++){
+//     console.log("Hello to ",args[i]);
+// }
+
+// const math = require("./math");
+// console.log(math);
+
+// const info = require("./Fruits");
+
+// console.log(info);
+// console.log(info[0].name);
+// console.log(info[1].name);
+// console.log(info[2].name);
---
+++ math.js
@@ -0,0 +1,12 @@
+const sum = (a,b) => a+b;
+const mul = (a,b) => a*b;
+const g = 9.8;
+const PI = 3.14;
+
+let obj = {
+    sum : sum,
+    mul : mul,
+    g : g,
+    PI : PI
+};
+module.exports = obj;
