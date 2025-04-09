0925e8d1bf414145fa5049bbd7f5412718e28edf
Mr_Shubham_Khalane <118313700+1khalaneshubham@users.noreply.github.com>
2025-04-09 17:12:04 +0530

    Add files via upload

--- math.js
+++ math.js
@@ -1,12 +1,26 @@
 const sum = (a,b) => a+b;
-const mul = (a,b) => a*b;
-const g = 9.8;
-const PI = 3.14;
+ const mul = (a,b) => a*b;
+ const g = 9.8;
+ const PI = 3.14;
 
-let obj = {
-    sum : sum,
-    mul : mul,
-    g : g,
-    PI : PI
-};
-module.exports = obj;
+//  option-1
+ let obj = {
+    sum:sum,
+    mul:mul,
+    g:g,
+    PI:PI,
+ };
+
+ module.exports  = obj;
+
+//  option-2:direct use of module.exports
+module.exports.Suum = (a,b) => a+b;
+module.exports.mul = (a,b) => a*b;
+module.exports.g = 9.8;
+module.exports.PI = 3.14;
+
+//  option-3: direct use of exports
+exports.Suum = (a,b) => a+b;
+exports.mul = (a,b) => a*b;
+exports.g = 9.8;
+exports.PI = 3.14;
---
+++ script.js
@@ -0,0 +1,28 @@
+// step-1 
+// let n = 5;
+
+// for(let i = 0; i<n; i++){
+//     console.log("Hello,",i); 
+// }
+
+// console.log("By..!");
+
+// step-2
+// let args = process.argv;
+
+//  for(let i = 2; i<args.length; i++){
+//     console.log("Hello mr.",args[i]);
+//  }
+//  console.log("Oky we meet soon");
+
+// step-3
+// const math = require("./math");
+
+// console.log(math.sum(10,12));
+// console.log(math.mul(2,4));
+// console.log(math.g);
+// console.log(math.PI);
+
+// step-4
+const info = require("./Fruits");
+console.log(info);
